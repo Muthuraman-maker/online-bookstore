716b8b6bedfe4ae9c618e11506ebcae1b54cfa15
Laptop <Laptop@Techie>
2020-03-22 17:45:37 +0530

    Created angular project and completed the feature 1

--- onlinebookstore/src/main/java/in/Muthuthetechie/onlinebookstore/config/RepositoryConfig.java
+++ onlinebookstore/src/main/java/in/Muthuthetechie/onlinebookstore/config/RepositoryConfig.java
@@ -10,19 +10,23 @@ import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 
 @Configuration
 public class RepositoryConfig implements RepositoryRestConfigurer{
-	
+	/*Autowiring feature of spring framework enables you to inject the object dependency implicitly. 
+	 * It internally uses setter or constructor injection. 
+	 * Autowiring can't be used to inject primitive and string values. 
+	 * It works with reference only
+	 * */
 	@Autowired
 	private EntityManager entityManager;
-	
-@Override
-public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config){
-	/*config.exposeIdsFor(Book.class);
+
+	@Override
+	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config){
+		/*config.exposeIdsFor(Book.class);
 	config.exposeIdsFor(BookCategory.class);*/
-	/*Instead of adding the entities
-	 * once by one
-	 * generically we are using the entityManger*/
-	config.exposeIdsFor(entityManager.getMetamodel().getEntities().stream()
-			.map(Type::getJavaType).toArray(Class[]::new));
-}
-	
+		/*Instead of adding the entities
+		 * once by one
+		 * generically we are using the entityManger*/
+		config.exposeIdsFor(entityManager.getMetamodel().getEntities().stream()
+				.map(Type::getJavaType).toArray(Class[]::new));
+	}
+
 }
